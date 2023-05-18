0f9fc6a3fdbec436d87839aec563dc66038b67aa
AmanTheGreatCoder <amanwadhwaniya159@gmail.com>
2023-05-18 19:54:37 +0530

    Commit!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
-
 import MuiPhoneNumber from './components/index'
 
 const App = props => {
   return (
     <div>
-        <MuiPhoneNumber/>
+      <MuiPhoneNumber defaultCountry={'af'} />
     </div>
   )
 }
